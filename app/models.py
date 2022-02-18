f9525ae2d54c68905b9e72b3c0bfb54372c4cc14
RhonaJoyKe <rhonajoy8@gmail.com>
2022-02-18 10:58:17 +0300

    adds user models

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    quizzes = models.ManyToManyField(Quiz, through='TakenQuiz')
+    interests = models.ManyToManyField(Subject, related_name='interested_students')
+
+# Create your models here.
